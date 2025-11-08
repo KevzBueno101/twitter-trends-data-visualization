609e68d824130f0b7abbb1d82554b120fb48b059
Kevin Bueno <KevzBueno101@users.noreply.github.com>
2025-11-08 18:58:36 +0800

    Fixed the code alignment using shift+alt+f

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let currentDate = new Date();
 
 dateElement.innerHTML = currentDate;
 
-let dateOptions = { year: 'numeric', month: 'short', day: 'numeric'};
+let dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
 
-dateElement.innerHTML = currentDate.toLocaleDateString('en-US', dateOptions); 
+dateElement.innerHTML = currentDate.toLocaleDateString('en-US', dateOptions);
 
 const url = 'https://twitter-trends5.p.rapidapi.com/twitter/request.php';
 const options = {
@@ -15,87 +15,87 @@ const options = {
 		'x-rapidapi-host': 'twitter-trends5.p.rapidapi.com',
 		'Content-Type': 'application/x-www-form-urlencoded'
 	},
-	body: new URLSearchParams({woeid: '23424977'})
+	body: new URLSearchParams({ woeid: '23424977' })
 };
 
 let graphData = [];
 
 fetch(url, options)
-.then(res => res.json())
-.then(data => {
-	console.log(data)
+	.then(res => res.json())
+	.then(data => {
+		console.log(data)
 
-	for (let i = 0; i < 15; i++){
-		graphData.push(
+		for (let i = 0; i < 15; i++) {
+			graphData.push(
 				{
 					"name": data.trends[i].name,
 					"volume": data.trends[i].volume
 				}
 			)
-	}
+		}
 
-	//map method
-	let topics = graphData.map(object => {
-		console.log(object);
-		console.log(object.name);
-		return object.name;
-	})
+		//map method
+		let topics = graphData.map(object => {
+			console.log(object);
+			console.log(object.name);
+			return object.name;
+		})
 
-	console.log(topics);
+		console.log(topics);
 
-	let volumes = graphData.map(object => {
-		return object.volume
-	})
+		let volumes = graphData.map(object => {
+			return object.volume
+		})
 
-	console.log(volumes);
+		console.log(volumes);
 
 
 
-	const myChart = document.getElementById('myChart');
+		const myChart = document.getElementById('myChart');
 
-	let barChart = new Chart(myChart, {
-	        type: 'bar',
-	        data: {
-	          labels: topics,
-	          datasets: [{
-	            label: '# of tweets/xeets',
-	            data: volumes,
-	            borderWidth: 2,
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(255, 159, 64, 0.2)',
-	                'rgba(255, 205, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(201, 203, 207, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgb(255, 99, 132)',
-	                'rgb(255, 159, 64)',
-	                'rgb(153, 102, 255)',
-	                'rgb(201, 203, 207)'
-	            ],
-	            hoverBackgroundColor: [
-	                'rgb(255, 99, 132)',
-	                'rgb(255, 159, 64)',
-	                'rgb(255, 205, 86)',
-	                'rgb(75, 192, 192)',
-	                'rgb(54, 162, 235)',
-	                'rgb(153, 102, 255)',
-	                'rgb(201, 203, 207)'
-	            ]
-	          }]
-	        },
-	        options: {
-	          indexAxis: 'y',
-	          scales: {
-	            y: {
-	              beginAtZero: true
-	            }
-	          }
-	        }
-	    });
+		let barChart = new Chart(myChart, {
+			type: 'bar',
+			data: {
+				labels: topics,
+				datasets: [{
+					label: '# of tweets/xeets',
+					data: volumes,
+					borderWidth: 2,
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(255, 159, 64, 0.2)',
+						'rgba(255, 205, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(201, 203, 207, 0.2)'
+					],
+					borderColor: [
+						'rgb(255, 99, 132)',
+						'rgb(255, 159, 64)',
+						'rgb(153, 102, 255)',
+						'rgb(201, 203, 207)'
+					],
+					hoverBackgroundColor: [
+						'rgb(255, 99, 132)',
+						'rgb(255, 159, 64)',
+						'rgb(255, 205, 86)',
+						'rgb(75, 192, 192)',
+						'rgb(54, 162, 235)',
+						'rgb(153, 102, 255)',
+						'rgb(201, 203, 207)'
+					]
+				}]
+			},
+			options: {
+				indexAxis: 'y',
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
 	})
 
 //Dummy data to comment out
